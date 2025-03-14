3b0bb9dcace809ed7fe70624b3865242a243c684
MeadShalehin <md.meadshalehin@gmail.com>
2025-03-14 15:56:45 +0600

    home section updated

---
+++ lib/utils/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+
+  static const Color primaryColor = Color(0xFF5B9C98);
+  static const Color primaryColor2 = Color(0xFF7FB5B1);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color whiteColor2 = Color(0xFF888888);
+  static const Color blackColor = Color(0xFF000000);
+
+}
